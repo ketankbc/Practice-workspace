3a241e501699d4f748ae4d46db2deaacc2b8da0e
Ketan <Ketan@ketans-MacBook-Pro.local>
2020-05-09 08:59:47 +0300

    added a code

--- Python/5_excpetio.py
+++ Python/5_excpetio.py
@@ -1,4 +1,9 @@
-#test
-
-
-oky
+def get_number():
+  try:
+    number1 =input('Enter a number. I will square...')
+  except:
+    print('please input a number')
+  else:
+    print('suare='+number1**2)
+  finally:
+    print("End of Program... Thank you..")
