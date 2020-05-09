f31ce681f98b857517f0564a4efe522a836c0f58
Ketan <Ketan@ketans-MacBook-Pro.local>
2020-05-10 01:00:54 +0300

    done homework

--- Python/5_excpetio.py
+++ Python/5_excpetio.py
@@ -7,3 +7,36 @@ def get_number():
     print('suare='+number1**2)
   finally:
     print("End of Program... Thank you..")
+
+
+#Handle the exception thrown by the code below by using try and except blocks.
+
+try:
+  for i in ['a','b','c']:
+    print(i**2)
+
+  x = 5
+  y = 0
+
+  z = x/y
+
+except TypeError:
+    print("This is a type error...please check values..")
+except:
+    print("Caught an exception... cannot devideby zero")
+finally:
+  print('all Done')
+
+#Write a function that asks for an integer and prints the square of it. Use a while loop with a try, except, else block to account for incorrect inputs.
+def ask():    
+      while True:
+        try:
+          number=int(input("Please input a number...") )
+        except:
+          print('Please re enter a number...')
+        else:
+          print('our number is '+str(number))
+          break
+
+
+ask()
