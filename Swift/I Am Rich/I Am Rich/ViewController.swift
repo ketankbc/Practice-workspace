7f98e96134272db06b59fd0218fc5d1bfb22a230
Ketan Chavan <ketan@Ketans-MacBook-Pro.local>
2020-12-13 13:22:18 +0300

    Swift projects added

---
+++ Swift/I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Ketan Chavan on 10/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
